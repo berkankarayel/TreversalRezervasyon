b6f3235f7bf77c2ea7a8336a2e6b6335a27fbfb3
berkankarayel <berkankarayel.147@gmail.com>
2025-10-20 22:43:58 +0300

    Add project files.

---
+++ TreversalRezervasyon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TreversalRezervasyon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
