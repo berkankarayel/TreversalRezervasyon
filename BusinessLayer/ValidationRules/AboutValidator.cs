e63fcd109a9a30cd64dc3644b1472dc9be36af67
berkankarayel <berkankarayel.147@gmail.com>
2025-10-23 18:26:43 +0300

    tema giydirildi. data acces katmanı genel mimari oluşturuldu business katmanı işlemlerine başlandı

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public  class AboutValidator:AbstractValidator<About>
+
+    {
+        public AboutValidator()
+        {
+                RuleFor(x=>x.Description).NotEmpty().WithMessage("Açıklama kısmı boş geçilemez");
+                RuleFor(x=>x.Image1).NotEmpty().WithMessage("Lütfen görsel ekleyiniz");
+                RuleFor(x=>x.Description).MinimumLength(50).WithMessage("Lütfen en az 50 karakter giriniz");
+                RuleFor(x=>x.Description).MaximumLength(1500).WithMessage("Lütfen en fazla 1000 karakter giriniz");
+
+        }
+    }
+}
